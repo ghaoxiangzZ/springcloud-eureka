45f51bc592a73dc6e49c3ae3759ca4f31937c0c9
ghaoxiang <784390929@qq.com>
2019-01-10 15:22:19 +0800

    springcloud-eureka

---
+++ eureka-client/src/main/java/com/ghaoxiang/client/web/EurekaClientController.java
@@ -0,0 +1,38 @@
+/*
+* 文件名: com.ghaoxiang.client.web
+* 文件编号: 
+* 版权: Copyright (c) 2019, YAN Co.Ltd. and/or its affiliates. All rights reserved.Use is subject to license terms.
+* 描述: 
+* 创建人: ghaoxiang
+* 创建时间: 2019年01月10日 15:14
+* 修改人:
+* 修改时间: 2019年01月10日 15:14
+* 修改变更号: 
+* 修改内容: 
+*/
+package com.ghaoxiang.client.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ghaoxiang
+ * @version V1.0
+ * @Title EurekaClientController
+ * @Description
+ * @date 2019年01月10日 15:14
+ * @since V1.0
+ */
+@RestController
+public class EurekaClientController {
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "ghaoxiangzZ") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+}
---
+++ src/main/java/com/ghoxiang/eureka/SpringcloudEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.ghoxiang.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudEurekaApplication.class, args);
+	}
+
+}
+
